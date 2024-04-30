cb37d3d95fdff721e8b9cd9fb374dc52f237fed0
A.R.M.Kiyas <62748979+ARMkiyas@users.noreply.github.com>
2024-05-01 01:11:08 +0530

    Update types.ts

--- utils/types.ts
+++ utils/types.ts
@@ -2,13 +2,9 @@ import { sendEmailOTPT } from "../src/handlers/OTP/sendEmailOTP"
 import { sendMessageOTPT } from "../src/handlers/OTP/sendmessageOTP"
 
 
-
-
-
 type OTPdataT = sendEmailOTPT | sendMessageOTPT
 
 type reqT = "otp" | "pwd-reset" | "appointment"
-
 
 export type item<T> = {
     type: reqT
